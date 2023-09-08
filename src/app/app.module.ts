57fcabb54709e6c8868864ca0b0f201b26ce6f64
Victor luiz <victorluizheredia@gmail.com>
2023-09-08 01:25:22 -0300

    [config] angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { HomeModule } from './pages/home/home.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     LayoutModule,
     HomeModule,
     AppRoutingModule,
